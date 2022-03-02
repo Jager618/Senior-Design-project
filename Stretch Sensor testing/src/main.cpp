3f7dc113fa412c9805db6b087aa1ced0007b8d25
Kyle <35355439+Jager618@users.noreply.github.com>
2022-03-02 10:39:55 -0800

    Update main.cpp

--- Stretch Sensor testing/src/main.cpp
+++ Stretch Sensor testing/src/main.cpp
@@ -1,17 +1,18 @@
-// Testing stretch sensors
+// Testing stretch sensors on ESP32
 #include <Arduino.h>
 
 int stretch_value = 0;
-int input_pin = 36; //GPIO pin 36
+int output_pin = 36; //GPIO pin 36 or we can use 3.3v output pin
+int feedback_input = 39; //GPIO pin 39
 
 void setup() {
    Serial.begin(155200);
 }
 
 void loop() {
-  stretch_value = analogRead(input_pin); //get adc value for pin
+  stretch_value = analogRead(feedback_input); //get adc value for pin
   delay(1000);
-
+   
   Serial.println(stretch_value);
   delay(1000);
 }
